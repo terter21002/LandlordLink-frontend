9a001fc5904965a5fcae99d56c86b2ac93f25243
tedluck <tedluck0105@gmail.com>
2024-05-09 22:36:22 -0600

    frontend 5th version

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -15,15 +15,6 @@ const blogPosts = [
     },  {
         title: 'First Post',
         content: 'This is the content of the firsts post.'
-    },  {
-        title: 'First Post',
-        content: 'This is the content of the firsts post.'
-    },  {
-        title: 'First Post',
-        content: 'This is the content of the firsts post.'
-    },  {
-        title: 'First Post',
-        content: 'This is the content of the firsts post.'
     },
 
     // Add more posts as needed
@@ -50,18 +41,62 @@ const Services: React.FC = () => {
                         </div>
                     ))}
                 </div>
-            </div>
-            <div className='col-span-5 tail-w'>
-                <h1 className="text-3xl font-bold text-black-800 ml-6 font-bold my-8">Management</h1>
-                <h3 className='text-gray-700 font-bold my-3 text-lg'>Property Management</h3>
+                <h3 className='text-gray-700 font-bold my-3 text-lg'>Background and Criminal Check</h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {blogPosts.map((post, index) => (
-                        <div key={index}>
-                            <div className="bg-white p-6 text-black-400 font-bold text-center rounded-md shadow-md mb-8 flex items-center">
-                                First Adgantage
+                  
+                        <div className='h-24 mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">First Advantage
+                                </h2>
                             </div>
                         </div>
-                    ))}
+                        <div className='h-24 mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">Innago
+                                </h2>
+                            </div>
+                        </div>
+                        <div className='h-24 mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">SmartMove
+                                </h2>
+                            </div>
+                        </div>
+                </div>
+                <h3 className='text-gray-700 font-bold my-3 text-lg flex justify-between'><span>Financial Services</span><span>Real Estate Consulting</span></h3>
+                <div className="h-28 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                  
+                        <div className='mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">Roofstocks</h2>
+                            </div>
+                        </div>
+                        <div className='mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">Insuarance Plan
+                                </h2>
+                            </div>
+                        </div>
+                        <div className='mx-auto w-full'>
+                            <div className="bg-white p-6 rounded-md shadow-md mb-8">
+                                <h2 className="text-xl font-bold mb-2">Smita Singh
+                                </h2>
+                            </div>
+                        </div>
+                </div>
+                
+            </div>
+            
+            <div className='col-span-5 tail-w mt-24'>
+                <h3 className='text-gray-700 font-bold my-3 text-lg'>Other Info</h3>
+                <div className="border w-full my-3 h-36 flex items-center justify-center text-2xl text-gray-500 font-bold">
+                      Tenant Search
+                </div>
+                <div className="border w-full my-3 h-36 flex items-center justify-center text-2xl text-gray-500 font-bold">
+                      Eviction Attorney
+                </div>
+                <div className="border w-full h-36 flex items-center justify-center text-2xl text-gray-500 font-bold">
+                       Eviction Service
                 </div>
             </div>
         </div>
